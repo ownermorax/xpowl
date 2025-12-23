c70f234079ed4d5409eea7304957a3de59e01214
morax <xpowl.t.me>
2025-12-23 21:58:08 +0300

    fix help()

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="xpowl",
-    version="1.0.1",
+    version="1.0.2",
     author="Morax",
     author_email="bloodlustedx@gmail.com",
     description="https://github.com/ownermorax/xpowl",
--- xpowl/__init__.py
+++ xpowl/__init__.py
@@ -1,9 +1,9 @@
 """
-Версия 1.0.1
+Версия 1.0.2
 """
 
 from .core import febub, ewrip, abs, is_prime, divisors
 
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 __author__ = "Morax"
 __all__ = ['abs', 'is_prime', 'divisors', 'febub', 'ewrip']
